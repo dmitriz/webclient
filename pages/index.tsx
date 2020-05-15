64fd5e45eebb888207c3a3198be99321c1a7afdc
Tobias Hegemann <tobias@Sarahs-MBP.fritz.box>
2020-05-16 01:42:45 +0200

    current

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import StageViewer from "../components/StageViewer";
 import useStage from "../lib/digitalstage/useStage";
 import {Button} from "baseui/button";
+import {FormControl} from "baseui/form-control";
+import {Input} from "baseui/input";
+import Container from "../components/ui/Container";
+import {styled} from "baseui";
+
+const Image = styled("img", {
+    display: 'block',
+    width: 'auto',
+    height: '100px',
+    objectFit: 'cover'
+});
+
+const Image2 = styled("img", {
+    display: 'block',
+    width: '100px',
+    height: 'auto',
+    objectFit: 'contain'
+});
 
 export default () => {
     const {create, join, stage} = useStage();
+    const [stageId, setStageId] = useState<string>();
 
     if (stage) {
         <StageViewer stage={stage}/>
     }
 
     return (
-        <div>
-            <Button onClick={() => join('mystageid')}>Join</Button>
-        </div>
+        <Container>
+            <h1><Image2
+                src="https://bilder.t-online.de/b/86/98/98/64/id_86989864/c_Master-1-1-Large/tid_da/jan-boehmermann-der-zdf-moderator-wechselt-2020-ins-zdf-hauptprogramm-seinem-sender-widmete-er-die-letzte-neo-magazin-royale-ausgabe-.jpg"/>
+                Late night stage ;-)</h1>
+            <Image
+                src="https://bilder.t-online.de/b/86/98/98/64/id_86989864/c_Master-1-1-Large/tid_da/jan-boehmermann-der-zdf-moderator-wechselt-2020-ins-zdf-hauptprogramm-seinem-sender-widmete-er-die-letzte-neo-magazin-royale-ausgabe-.jpg"/>
+
+            <FormControl label="Stage ID">
+                <Input value={stageId} onChange={(e) => setStageId(e.currentTarget.value)}/>
+            </FormControl>
+            <Button onClick={() => join('APDCnkEtjiNYAkEkQoEG')}>Join</Button>
+        </Container>
     )
 }
